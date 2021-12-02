b35d4323d7ddfe458df8d84085b2d021a2e0fd64
dwisyafriliaa <92431521+dwisyafriliaa@users.noreply.github.com>
2021-12-02 23:54:51 +0700

    Add files via upload

---
+++ Calculator/Calculator/Calculator_Hasil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator_Hasil : Form
+    {
+        public Calculator_Hasil()
+        {
+            InitializeComponent();
+        }
+
+        private void Output(int operasi, int a, int b, int hasil)
+        {
+            char[] operasi_simbol = { '+', '-', '*', '/' };
+            string[] operasi_str = { "Penambahan", "Pengurangan", "Perkalian", "Pembagian" };
+            lstHasil.Items.Add($"Hasil {(operasi_str[operasi]).ToLower()} {a} {operasi_simbol[operasi]} {b} = {hasil}");
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            Calculator_Input cal = new Calculator_Input();
+            cal.Create += Output;
+            cal.ShowDialog();
+        }
+    }
+}
---
+++ Calculator/Calculator/Calculator_Input.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator_Input : Form
+    {
+        public delegate void CreateData(int operasi, int a, int b, int hasil);
+        public event CreateData Create;
+
+        public Calculator_Input()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            string operasi = cbOperasi.SelectedItem.ToString();
+            int a = Convert.ToInt32(txtNilaiA.Text);
+            int b = Convert.ToInt32(txtNilaiB.Text);
+            int hasil = 0;
+
+            switch (cbOperasi.SelectedIndex)
+            {
+                case 0:
+                    hasil = a + b;
+                    break;
+                case 1:
+                    hasil = a - b;
+                    break;
+                case 2:
+                    hasil = a * b;
+                    break;
+                case 3:
+                    hasil = a / b;
+                    break;
+                default:
+                    hasil = 0;
+                    break;
+            }
+            this.Create(cbOperasi.SelectedIndex, a, b, hasil);
+        }
+    }
+}
